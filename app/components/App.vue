c3c35dd4f6eec3d575898aa859c53f84003bdd90
netlesh <netlesh@mail.ru>
2020-06-04 15:20:41 +0300

    boyut göstergesi

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,12 +1,15 @@
 <template>
   <Page>
     <ActionBar>
-      <Label class="title" text="Fotoğraf Ekleme" col="1" />
+      <Label class="title" text="300 px Fotoğraf Ekleme" col="1" />
     </ActionBar>
 
-    <GridLayout columns="*" rows="330,60">
+    <GridLayout columns="*" rows="330,60,60,60">
       <Image :src="photo" stretch="aspectFill" row="0" height="300" width="300" />
-      <Button text="foto çek" @tap="take" row="1" />
+      <!-- <Image :src="file" stretch="aspectFill" row="0" height="300" width="300" /> -->
+      <Button text="foto çek" @tap="take" row="1" class="p-5 font-weight-bold" />
+      <Label v-show="boyut.length > 0" :text="'Resim boyutu: ' + boyut" textWrap="true" row="2" class="text-center h3 p-5" />
+      <Button v-show="photo" text="Gönder" @tap="send" row="3" class="font-weight-bold" />
     </GridLayout>
   </Page>
 </template>
@@ -16,11 +19,15 @@ import * as camera from "nativescript-camera";
 import { Image } from "tns-core-modules/ui/image";
 import { ImageSource, fromFile, fromResource, fromBase64 } from "tns-core-modules/image-source";
 import { Folder, path, knownFolders } from "tns-core-modules/file-system";
+import { isAndroid, isIOS } from "tns-core-modules/platform";
+
 export default {
   data() {
     return {
-      arrayPictures: [],
+      // arrayPictures: [],
       photo: null,
+      photoPath: "",
+      boyut: "",
     };
   },
   methods: {
@@ -33,6 +40,7 @@ export default {
         })
         .then((imageAsset) => {
           console.log("Size: " + imageAsset.options.width + "x" + imageAsset.options.height);
+          this.boyut = imageAsset.options.width + "x" + imageAsset.options.height;
           console.log("keepAspectRatio: " + imageAsset.options.keepAspectRatio);
           // SAVE IMAGE
           ImageSource.fromAsset(imageAsset).then((imageSource: ImageSource) => {
@@ -42,6 +50,7 @@ export default {
             const saved: boolean = imageSource.saveToFile(filePath, "jpg");
             if (saved) {
               console.log("Saved: " + filePath);
+              this.photoPath = filePath;
               console.log("Image saved successfully!");
             } else {
               console.log("not saved");
@@ -55,6 +64,93 @@ export default {
           console.log("Error -> " + err.message);
         });
     },
+    send() {
+      console.log("this.photoPath :>> ", this.photoPath);
+      console.log("this.photo :>> ", this.photo);
+    },
+    /*     takeResize() {
+      
+      camera
+        .takePicture({
+          width: 500, //these are in device independent pixels
+          height: 500, //only one will be respected depending on os/device if
+          keepAspectRatio: true, //    keepAspectRatio is enabled.
+          saveToGallery: false, //Don't save a copy in local gallery, ignored by some Android devices
+        })
+        .then((imageAsset) => {
+          imageAsset.options.autoScaleFactor = false;
+          imageAsset.options.keepAspectRatio = true;
+          ImageSource.fromAsset(imageAsset).then((imageSource) => {
+            var ratio = 1,
+              newheight = imageSource.height,
+              newwidth = imageSource.width;
+            if (imageSource.width > 500 && imageSource.height > 500) {
+              if (imageSource.width > imageSource.height) {
+                newwidth = 500;
+                newheight = Math.round(500 * (imageSource.height / imageSource.width));
+              } else {
+                newheight = 500;
+                newwidth = Math.round(500 * (imageSource.width / imageSource.height));
+              }
+            }
+            if (imageSource.width > 500 && imageSource.height > 500) {
+              if (isAndroid) {
+                try {
+                  var downsampleOptions = new android.graphics.BitmapFactory.Options();
+                  var bitmap = android.graphics.BitmapFactory.decodeFile(imageAsset.android, null);
+                  var newBitmap = android.graphics.Bitmap.createScaledBitmap(bitmap, newwidth, newheight, true);
+                  imageSource.setNativeSource(newBitmap);
+                  let filename = "image-" + new Date().getTime() + ".jpg";
+                  const folderNew = knownFolders.documents().path;
+                  let realPath = path.join( folderNew , filename);
+                  imageSource.saveToFile(realPath, "jpeg");
+                  // imageSource.filename = filename;
+                  // imageSource.note = "";
+                  this.photo = imageSource;
+                  // that.storeData();
+                } catch (err) {
+                  console.error(err);
+                }
+              } else {
+                let manager = PHImageManager.defaultManager();
+                let options = new PHImageRequestOptions();
+                options.resizeMode = PHImageRequestOptionsResizeMode.Exact;
+                options.deliveryMode = PHImageRequestOptionsDeliveryMode.HighQualityFormat;
+                manager.requestImageForAssetTargetSizeContentModeOptionsResultHandler(
+                  imageAsset.ios,
+                  { width: newwidth, height: newheight },
+                  PHImageContentMode.AspectFill,
+                  options,
+                  function(result, info) {
+                    let folder = knownFolders.documents().path;
+                    let filename = "image-" + new Date().getTime() + ".jpg";
+                    let realPath = path.join(folder, filename);
+                    const loadedImage: ImageSource = ImageSource.fromFileSync(realPath)
+                    // let newasset = imageAsset(result);
+
+                    ImageSource.fromAsset(loadedImage).then((newimageSource) => {
+                      newimageSource.saveToFile(realPath, "jpeg");
+                      // newimageSource.filename = filename;
+                      // newimageSource.note = "";
+                      that.arrayPictures.unshift(newimageSource);
+                      that.storeData();
+                    });
+                  }
+                );
+              }
+            } else {
+              let filename = "image" + "-" + new Date().getTime() + ".jpg";
+              let folder = fsModule.knownFolders.documents();
+              let path = fsModule.path.join(folder.path, filename);
+              imageSource.saveToFile(path, "jpeg");
+              imageSource.filename = filename;
+              imageSource.note = "";
+              that.arrayPictures.unshift(imageSource);
+              that.storeData();
+            }
+          });
+        });
+    }, */
   },
   mounted() {
     camera.requestPermissions().then(
